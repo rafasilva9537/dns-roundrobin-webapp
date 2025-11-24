fea2df49c00f3a124285c6ed615ed7aca7bbd3ad
Rafael <rafasilva9537@gmail.com>
2025-11-24 10:14:53 -0300

    Add XML documentation for IDateTimeProvider and ITokenService interfaces

--- backend/api/Interfaces/Services/IDateTimeProvider.cs
+++ backend/api/Interfaces/Services/IDateTimeProvider.cs
@@ -1,6 +1,13 @@
 namespace api.Interfaces.Services;
 
+/// <summary>
+/// Provides an abstraction for accessing date and time of type <see cref="DateTime"/>.
+/// Useful for ensuring consistent time references across the application
+/// and for facilitating unit testing by allowing the date and time
+/// to be mocked or substituted.
+/// </summary>
 public interface IDateTimeProvider
 {
+    /// Gets the current date and time in Coordinated Universal Time (UTC).
     DateTime UtcNow { get; }
 }
--- backend/api/Interfaces/Services/ITokenService.cs
+++ backend/api/Interfaces/Services/ITokenService.cs
@@ -2,7 +2,17 @@ using api.Entities;
 
 namespace api.Interfaces.Services;
 
+/// <summary>
+/// Provides functionality for generating authentication tokens for users.
+/// </summary>
 public interface ITokenService
 {
+    /// <summary>
+    /// Generates a JSON Web Token (JWT) for the specified user, embedding user claims and other token-related properties.
+    /// </summary>
+    /// <param name="user">The user for whom the token is being generated. This includes user information such as username and email.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the generated JWT as a string.
+    /// </returns>
     Task<string> GenerateToken(User user);
 }
